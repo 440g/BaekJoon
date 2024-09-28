1dc834ce1d1767bc273a375d1bd0e4c8d607ea63
MINSEO KIM <meanwestk@gmail.com>
2024-09-28 13:39:38 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 6.06 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/1/258712. 가장 많이 받은 선물/README.md
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/README.md
@@ -1,10 +1,10 @@
 # [level 1] 가장 많이 받은 선물 - 258712 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/258712?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/258712) 
 
 ### 성능 요약
 
-메모리: 10.6 MB, 시간: 8.67 ms
+메모리: 10.6 MB, 시간: 6.06 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 27일 20:49:56
+2024년 09월 28일 13:39:32
 
 ### 문제 설명
 
--- 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.py
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.py
@@ -1,31 +1,33 @@
+import sys
+input = sys.stdin.readline
+
 def solution(friends, gifts):
+    #초기화
     n = len(friends)
     d = dict()
-    gives = [[0] * n for _ in range(n)]
-    giftScore = [0] * n
-    giftProm = [0] * n
-    
-    for i in range(n):
+    give = [[0] * n for _ in range (n)]
+    giftscore = [0] * n
+    giftpromised = [0] * n
+    for i in range (n):
         d[friends[i]] = i
-    
     for i in range(len(gifts)):
         a, b = gifts[i].split()
         a = d[a]
         b = d[b]
+        give[a][b] += 1
+        giftscore[a] += 1
+        giftscore[b] -= 1
         
-        gives[a][b] += 1
-        giftScore[a] += 1
-        giftScore[b] -= 1
-    # 초기화 
-        
+    # 코드 
     for i in range(n):
         for j in range(n):
-            if i==j:
+            if i == j:
                 continue
             
-            if gives[i][j] > gives[j][i]:
-                giftProm[i] += 1
-            elif (gives[i][j] == gives[j][i]) and (giftScore[i] > giftScore[j]):
-                giftProm[i] += 1 
-                
-    return max(giftProm)
+            if give[i][j] > give[j][i]:
+                giftpromised[i] += 1
+            elif (give[i][j] == give[j][i]) and giftscore[i] > giftscore[j]:
+                giftpromised[i] += 1
+    
+    return max(giftpromised)    
+    
